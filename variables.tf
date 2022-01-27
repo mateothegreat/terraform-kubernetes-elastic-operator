55c90f424f8eb0e9f8a75f166abd1c9356c6562d
Matthew Davis <matthew@matthewdavis.io>
2022-01-27 09:31:44 -0500

    bump

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
-variable "host" {
-
-    type        = string
-    description = "kuberetes api url"
-
-}
-
-variable "token" {
-
-    type        = string
-    description = "kubernetes api token"
-
-}
-
-variable "operator_url" {
-
-    type = string
-    description = "elastic operator url"
-    default = "https://download.elastic.co/downloads/eck/1.4.0/all-in-one.yaml"
-
-}
+#variable "host" {
+#
+#    type        = string
+#    description = "kuberetes api url"
+#
+#}
+#
+#variable "token" {
+#
+#    type        = string
+#    description = "kubernetes api token"
+#
+#}
+#
+#variable "operator_url" {
+#
+#    type        = string
+#    description = "elastic operator url"
+#    default     = "https://download.elastic.co/downloads/eck/1.9.1/all-in-one.yaml"
+#
+#}
